a06c4f12b433dc59b6b1c792db5a579782b61c57
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-18 18:17:58 +0200

    1

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,14 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  name: '',
+};
+
 const filtersSlice = createSlice({
   name: 'filters',
-  initialState: { name: '' },
+  initialState,
   reducers: {
-    changeFilter(state, action) {
+    changeFilter: (state, action) => {
       state.name = action.payload;
     },
   },
 });
 
 export const { changeFilter } = filtersSlice.actions;
+
+export const selectNameFilter = (state) => state.filters.name;
+
 export default filtersSlice.reducer;
